854671abe7f18e80665476e1ceef4d4537f468b6
amirphl <amirphl4@gmail.com>
2025-09-24 15:36:29 +0000

    Remove Weex Exchange

--- live_trading/exchanges/__init__.py
+++ live_trading/exchanges/__init__.py
@@ -1,6 +1,7 @@
 """Exchange implementations."""
 
 from .bitunix import BitunixExchange
-from .weex import WeexExchange, WeexTradingMode
 
-__all__ = ["BitunixExchange", "WeexExchange", "WeexTradingMode"]
+__all__ = [
+    "BitunixExchange",
+]
