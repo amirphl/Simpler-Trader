5b8bada7647fbc73f8993364f50385ede6931d3d
amirphl <amirphl4@gmail.com>
2026-05-04 08:46:45 +0000

    feat(experiments): export BOS/CHoCH and liquidity zone APIs

--- experiments/__init__.py
+++ experiments/__init__.py
@@ -1,1 +1,41 @@
-"""Experimental algorithms (pivots, liquidity zones, etc.)."""
+"""Experimental algorithms (pivots, BOS/CHoCH, liquidity zones, etc.)."""
+
+from experiments.bos_choch_detection import (
+    BOSCHoCHResult,
+    BOSRecord,
+    CHoCHUpdate,
+    DetectionConfig,
+    DetectionEvent,
+    DirectionState,
+    build_plotly_figure,
+    detect_bos_choch,
+    infer_initial_direction,
+)
+from experiments.liquidity_zone_detection import (
+    DirectionSegment,
+    LiquidityZone,
+    LiquidityZoneConfig,
+    LiquidityZoneResult,
+    PriceRange,
+    build_liquidity_zone_plotly_figure,
+    detect_liquidity_zones,
+)
+
+__all__ = [
+    "BOSCHoCHResult",
+    "BOSRecord",
+    "CHoCHUpdate",
+    "DetectionConfig",
+    "DetectionEvent",
+    "DirectionState",
+    "DirectionSegment",
+    "LiquidityZone",
+    "LiquidityZoneConfig",
+    "LiquidityZoneResult",
+    "PriceRange",
+    "build_plotly_figure",
+    "build_liquidity_zone_plotly_figure",
+    "detect_bos_choch",
+    "detect_liquidity_zones",
+    "infer_initial_direction",
+]
