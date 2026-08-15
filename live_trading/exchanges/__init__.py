2a8803d2b7b7443911e2f6c8c2206997b98c855d
amirphl <amirphl4@gmail.com>
2026-08-15 14:15:00 +0330

    Export Weex routing exchanges

--- live_trading/exchanges/__init__.py
+++ live_trading/exchanges/__init__.py
@@ -1,7 +1,11 @@
 """Exchange implementations."""
 
 from .bitunix import BitunixExchange
+from .routed import ZecWeexRoutedExchange
+from .weex import WeexExchange
 
 __all__ = [
     "BitunixExchange",
+    "WeexExchange",
+    "ZecWeexRoutedExchange",
 ]
