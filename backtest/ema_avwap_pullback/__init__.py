bb028c648cf54a865dc9921ccde95f9b01837598
amirphl <amirphl4@gmail.com>
2026-09-21 11:00:00 +0330

    Export EMA funding mode

--- backtest/ema_avwap_pullback/__init__.py
+++ backtest/ema_avwap_pullback/__init__.py
@@ -5,6 +5,7 @@ from .config import (
     EntryMode,
     ExitBand,
     ExitMode,
+    FundingMode,
 )
 
 __all__ = [
@@ -12,4 +13,5 @@ __all__ = [
     "EntryMode",
     "ExitMode",
     "ExitBand",
+    "FundingMode",
 ]
